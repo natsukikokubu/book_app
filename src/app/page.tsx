428a8ebcf4bffa3800c4ff116a68f7c0dfc5e530
natsukikokubu <natsukikokubu3@gmail.com>
2024-06-18 17:48:14 +0900

    get date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
+  const today = new Date();
+  const formattedDate = `${today.getFullYear()} / ${
+    today.getMonth() + 1
+  } / ${today.getDate()}`;
+  console.log(formattedDate);
+
   return (
     <main>
       <div>
+        {formattedDate}
         <p>Hello world!</p>
       </div>
     </main>
