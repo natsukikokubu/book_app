cd6b393e216495c2430bc8fda782e20a32fd103d
natsukikokubu <natsukikokubu3@gmail.com>
2024-06-25 22:41:43 +0900

    修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,33 @@
+import axios from "axios";
+import apiClient from "@/lib/apiClient";
+import { use, useEffect, useState } from "react";
+import { Books } from "./type";
+
 const today = new Date();
 const formattedDate = `${today.getFullYear()} / ${
   today.getMonth() + 1
 } / ${today.getDate()}`;
 
 export default function Home() {
-  return (
-    <main>
-      <div>
-        {formattedDate}
-        <p>Hello world!</p>
-      </div>
-    </main>
-  );
+  const [books, setBooks] = useState<Books[]>([]);
+  {
+    useEffect(() => {
+      async function fetchBooks() {
+        const data = await supabase.from("books").select("*");
+        if (data) {
+          setBooks(data);
+        } else {
+          console.error("Failed to fetch books");
+        }
+      }
+    }, []);
+    return (
+      <main>
+        <div>
+          {formattedDate}
+          <p>Hello world!</p>
+        </div>
+      </main>
+    );
+  }
 }
---
+++ src/app/type.tsx
@@ -0,0 +1,27 @@
+export type Books = {
+  id: string;
+  title: string;
+  summary: string;
+  report: string;
+  createdAt: string;
+};
+export type Book = {
+  id: string;
+  title: string;
+  summary: string;
+  report: string;
+  createdAt: string;
+};
+// export type BookInput = {
+//   title: string;
+//   summary: string;
+//   report: string;
+// };
+// export type BookUpdate = {
+//   title: string;
+//   summary: string;
+//   report: string;
+// };
+// export type BookDelete = {
+//   id: string;
+// };
